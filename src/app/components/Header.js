93080326be32f3ba6a22c0d40696b0dc3bccc42c
kashyapprajapat <prajapatikashyap14@gmail.com>
2025-03-24 17:12:43 +0530

    Feat:Header components complete

---
+++ src/app/components/Header.js
@@ -0,0 +1,10 @@
+
+export default function Header() {
+    return (
+      <header className="py-6 w-full text-center">
+        <h1 className="text-3xl font-bold bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent">
+          ShortReelX
+        </h1>
+      </header>
+    );
+  }
