af6000ed397bddd6e307095497057c0762579f9a
kashyapprajapat <prajapatikashyap14@gmail.com>
2025-03-24 17:13:10 +0530

    Feat:Footer components complete

---
+++ src/app/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+      <footer className="py-6 mt-auto text-center text-sm text-gray-500">
+        © 2025 by Kashyap Prajapati
+      </footer>
+    );
+  }
